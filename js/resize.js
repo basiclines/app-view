b494d7e604303043d98e9022dfa01bf706d83040
Ismael González <ismael@basiclines.com>
2012-06-02 13:52:29 +0200

    Rewrited scales factors and added resize event

--- js/resize.js
+++ js/resize.js
@@ -1,5 +1,25 @@
 //RESIZE.JS
 
+//Decimals split
+decimal_split = function ( number ) {
+
+	var fragment = function () {
+		if ( number.toString().split(".")[1] == undefined ) {
+			return 0;
+		} else {
+			var decimal_1 = number.toString().split(".")[1].charAt(0);
+			var decimal_2 = number.toString().split(".")[1].charAt(1);
+			var final_decimals = decimal_1 + decimal_2;
+
+			return final_decimals;
+		}
+	};
+
+	var aproximation = parseFloat(number.toString().split(".")[0] + "." +fragment());
+
+	return aproximation;
+};
+
 //Base set-up
 base =  {
 	font: 62.5,
@@ -21,50 +41,59 @@ device =  {
 	},
 	ratio: function () {
 		var raw = this.height() / this.width();
-		var fragment = function () {
-			if ( raw.toString().split(".")[1] == undefined ) {
-				return 0;
-			} else {
-				return raw.toString().split(".")[1].charAt(0);
-			}
-		};
-		var aprox = parseFloat(raw.toString().split(".")[0] + "." +fragment());
-
-		return aprox;
+		decimal_split(raw);
 	}
 }
 
 //Scale function
 scale =  (function (){
 	
-	var scale_ratio = base.width / device.width();
+	var scale_ratio = decimal_split(device.width() / base.width );
+
 	var correction_value = base.correction;
 
 	// Chek for non base ratio devices
-	if ( base.ratio() < device.ratio() ) {
-		var correction_value = base.correction - (scale_ratio * base.correction / 2);
-	}
+	// if ( base.ratio() < device.ratio() ) {
+	// 	var correction_value = base.correction - (scale_ratio * base.correction / 2);
+	// }
 
 	var root = document.getElementsByTagName("html")[0];
 	var font_size = base.font;
 
-	//Check for bigger devices
-	if (  base.width < device.width() ) {
-		var font_size = scale_ratio * base.font * correction_value; 
+	//Check for non base width devices
+	if (  base.width != device.width() ) {
+		var font_size = scale_ratio * base.font; 
 	}
 
-	//Check for smaller devices
-	if ( base.width > device.width() ) {
-		var font_size = scale_ratio * base.font * base.correction / 6; 
+	//Check for portrait devices
+	if ( device.width() == device.height() || device.width() > device.height() ) {
+		var font_size = device.width() / 1000 * base.font;
 	}
+
+
+
+	var font_size = decimal_split(font_size);
 
 	root.style.fontSize = font_size+"%";
 
-});
-
-//Add scale in window load event
-window.addEventListener("load", function (e) {
-
-	scale();
+	console.log("-.-.-.-.-.-.-.-.-.-.-.");
+	console.log(font_size + " font");
+	console.log(scale_ratio + " scale up ratio");
+	console.log(device.width() + " / " + device.height() + " device");
 
 });
+
+//Add scale to listeners
+window.addEventListener("load", function (e) { scale() });
+window.addEventListener("resize", function (e) { scale() });
+
+
+
+
+
+
+
+
+
+
+
